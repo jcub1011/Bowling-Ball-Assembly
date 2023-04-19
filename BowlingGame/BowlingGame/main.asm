521995da3eb8ea939585f800e90045f79301d586
Jacob McCormack <91000914+jcub1011@users.noreply.github.com>
2023-04-19 17:47:36 -0400

    Add files via upload
    Good luck.

--- BowlingGame/BowlingGame/main.asm
+++ BowlingGame/BowlingGame/main.asm
@@ -5,6 +5,12 @@
 ; Author : jcubm
 ;
 
+; Constants
+.equ ButtonDir = DDRD
+.equ ButtonReg = DDD2
+.equ ButtonPort = PORTD
+.equ ButtonPin = PD2
+
 ; Vector Table
 .org 0x0000				; Reset vector
 	jmp main
@@ -16,10 +22,35 @@
 ; End vector table.
 
 
-main:
-    inc r16
+main:     
+	; init stack
+     ldi  r16,HIGH(RAMEND)
+     out  SPH,r16
+     ldi  r16,LOW(RAMEND)
+     out  SPL,r16
 
+	; Init GPIO
+	cbi ButtonDir, ButtonReg ; Button pull-up.
+	sbi ButtonPort, ButtonPin
 
+	; Init Bowling Ball LEDS [b0-b2]
+	sbi	DDRB, DDB0
+	sbi	PORTB, PB0
+	sbi	DDRB, DDB1	
+	sbi	DDRB, DDB2		
+
+	; Init Interrupts
+	ldi	r16, (1<<INT0)
+	out	EIMSK, r16
+	ldi	r16, (1<<ISC01)
+	sts	EICRA, 16
+	sei					; Enable global interupts.
 
 main_loop:
+	; Bowling ball leds = [PD3, PD4, PD5]
+
+
+
+
+
     rjmp main_loop
